b7bac3536149e628d20e07cd8292e0f5fd9dff55
Millborne-g <galamiton.millborne@gmail.com>
2023-06-20 04:59:15 +0800

    commit

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,15 +1,33 @@
-import NextAuth from "next-auth"
+// import NextAuth from "next-auth"
+// import GoogleProvider from "next-auth/providers/google";
+
+// export const authOptions ={
+//   providers: [
+//     GoogleProvider({
+//       clientId: process.env.GOOGLE_CLIENT_ID,
+//       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+//     })
+//   ],
+//   secret: process.env.JWT_SECRET,
+// }
+
+// const handler = NextAuth(authOptions)
+// export {handler as GET, handler as POST}
+
+
+import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
-export const authOptions ={
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    })
-  ],
-  secret: process.env.JWT_SECRET,
-}
+const providers = [
+  GoogleProvider({
+    clientId: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  }),
+];
 
-const handler = NextAuth(authOptions)
-export {handler as GET, handler as POST}
+const options = {
+  providers,
+  secret: process.env.JWT_SECRET,
+};
+
+export default (req, res) => NextAuth(req, res, options);
